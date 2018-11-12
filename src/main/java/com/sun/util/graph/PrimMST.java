77a2d05f2d1e57a6309524de88a4a8dffadacb83
sunhuaquan <823045628@qq.com>
2018-11-12 23:23:05 +0800

    优化后的prim算法

---
+++ src/main/java/com/sun/util/graph/PrimMST.java
@@ -0,0 +1,15 @@
+package com.sun.util.graph;
+
+/**
+ * @author sunhuaquan
+ * @Title: PrimMST
+ * @ProjectName data-structure
+ * @Description: 优化后的prim算法
+ * @date 2018/11/1222:30
+ */
+public class PrimMST {
+
+
+
+
+}
